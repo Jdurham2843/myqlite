37b0cb91f6606bd3eec9365d06293a11b84db598
Durham, John <jdurham2843@gmail.com>
2024-12-24 19:51:23 -0600

    implement initial main function to take statements from stdin and print parsed structure

--- src/main/java/com/jdurham/Main.java
+++ src/main/java/com/jdurham/Main.java
@@ -1,7 +1,27 @@
 package com.jdurham;
 
+import com.jdurham.parsing.Parser;
+import com.jdurham.parsing.Statement;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Mqlite startup preamble");
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("> ");
+            final String input = scanner.nextLine();
+
+            if (input.equals("exit")) {
+                System.out.println("See ya!");
+                break;
+            }
+
+            final Parser parser = Parser.build(input);
+            final Statement statement = parser.parse();
+            System.out.println(statement);
+        }
     }
 }
